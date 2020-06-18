6a5cfa97736a01844f0761014b142ba9b7950a58
Paula <cedones@gmail.com>
2020-06-18 15:14:07 -0300

    Estilizando

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import Header from './components/Header.js';
-import styled from 'styled-components';
-import Wrapper from './components/Wrapper.js';
-import Table from './components/Table.js';
+import React from "react";
+import Header from "./components/Header.js";
+import styled from "styled-components";
+import Wrapper from "./components/Wrapper.js";
+import Table from "./components/Table.js";
+import Rules from "./components/Rules.js";
 
 const AppStyled = styled.main`
-  background-image: radial-gradient(circle at top, #1F3757 50%, #131537 100%);
-  min-height: 100vh;
-  padding: 2em;
+  background-image: radial-gradient(circle at top, #1f3757 50%, #131537 100%);
   color: white;
-  body{
-    font-family: 'Barlow Semi Condensed', sans-serif;
+
+  body {
+    font-family: "Barlow Semi Condensed", sans-serif;
   }
-`
+  .app-content {
+    padding: 2em;
+    min-height: 100vh;
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+`;
 
 function App() {
   return (
     <AppStyled>
-
       <Wrapper>
-        <Header/>
-        <Table/>
+        <div className="app-content">
+          <Header />
+          <Table />
+          <Rules />
+        </div>
       </Wrapper>
-       
     </AppStyled>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,60 @@
-import React from 'react';
-import styled from 'styled-components';
-import Token from './Token.js';
+import React from "react";
+import styled from "styled-components";
+import Token from "./Token.js";
 
 const TableStyled = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    
-`
+  display: grid;
+  grid-template-columns: 130px 130px;
+  justify-content: center;
+  justify-items: center;
+  grid-gap: 30px 50px;
+  margin: 2em 0;
+  position: relative;
+  & div:nth-of-type(3) {
+    grid-column: span 2;
+  }
+  .line {
+    height: 10px;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.2);
+    position: absolute;
+    left: 60px;
+    right: 60px;
+    top: 60px;
 
-function Table () {
-    return(
-        <TableStyled>
-            <Token name="rock" />
-            <Token name="scissors"/>
-            <Token name="paper"/>
-        </TableStyled>
-    )
+    &:before {
+      content: "";
+      height: 10px;
+      background: rgba(0, 0, 0, 0.2);
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      transform: rotate(60deg);
+      transform-origin: left top;
+    }
+    &:after {
+      content: "";
+      height: 10px;
+      background: rgba(0, 0, 0, 0.2);
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      transform: rotate(-60deg);
+      transform-origin: right top;
+    }
+  }
+`;
 
+function Table() {
+  return (
+    <TableStyled>
+      <span className="line"></span>
+      <Token name="paper" />
+      <Token name="scissors" />
+      <Token name="rock" />
+    </TableStyled>
+  );
 }
 export default Table;
