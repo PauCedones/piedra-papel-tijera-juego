9b86a056908decec720a361caba8d9a7285b9d8d
Paula <cedones@gmail.com>
2021-03-11 14:33:31 -0300

    Updates

---
+++ precache-manifest.2f6e1c2dfe56903d4b657034b70be352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8286040a0a66647d2fb76564609872",
+    "url": "/piedra-papel-tijera-juego/index.html"
+  },
+  {
+    "revision": "70cefaa4bcbce6678c78",
+    "url": "/piedra-papel-tijera-juego/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "52ab7984468e740ddd7e",
+    "url": "/piedra-papel-tijera-juego/static/js/2.ecc5ef6e.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/piedra-papel-tijera-juego/static/js/2.ecc5ef6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70cefaa4bcbce6678c78",
+    "url": "/piedra-papel-tijera-juego/static/js/main.b3720d36.chunk.js"
+  },
+  {
+    "revision": "1688f13bfae0ce4cee46",
+    "url": "/piedra-papel-tijera-juego/static/js/runtime-main.e7ee8299.js"
+  }
+]);
